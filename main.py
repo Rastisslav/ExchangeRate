b53cba8169b52bc89630a08d50669f2edc095f7b
Rastisslav <bolibruch2000@gmail.com>
2023-06-09 15:20:04 +0200

    finishing project

--- main.py
+++ main.py
@@ -4,26 +4,29 @@ from datetime import datetime, timedelta
 from modules.connect_db import connect_to_db, add_to_table, quit_db, show_history
 
 
-
+# funkcia ktora porovna exchangerate medzi dneskom a vcerajskom
 def check_exchange(command, year, month, day):
     left, right = command.split()
     api, boolean = Api(left.upper(), right.upper(), year, month, day)
-    
     return api, boolean, left, right
 
-
+# funkcia vrati aktualny cas a rok,mesiac,den ktory bol vcera
 def getDate():
     now = datetime.now()
     yesterday = datetime.now() - timedelta(days=1)
-    return now.strftime("%G-%m-%d %H:%M"), yesterday.strftime("%G"), yesterday.strftime("%m"), yesterday.strftime("%d")
+    return now.strftime("%G-%m-%d %H:%M"), yesterday.strftime(
+        "%G"), yesterday.strftime("%m"), yesterday.strftime("%d")
 
-def prRed(skk): return("\033[91m {}\033[00m" .format(skk))
- 
-def prGreen(skk): return("\033[92m {}\033[00m" .format(skk))
+# zmeni text do jednej z danych farieb
+def prRed(skk): return ("\033[91m {}\033[00m" .format(skk))
 
+
+def prGreen(skk): return ("\033[92m {}\033[00m" .format(skk))
+
+# zadavanie commandov, quit = vypne
+#                      history = vypise historiu
+#                      zvysok posiela api  
 def cli_commands():
-    
-    #print("prejde db")
     for command in sys.stdin:
         if 'quit' == command.rstrip():
             quit_db()
@@ -32,15 +35,14 @@ def cli_commands():
             result = show_history()
             for item in result:
                 if item[4]:
-                    print(item[0],item[1],item[2],prGreen(item[3]))
+                    print(item[0], item[1], item[2], prGreen(item[3]))
                 else:
-                    print(item[0],item[1],item[2],prRed(item[3]))
+                    print(item[0], item[1], item[2], prRed(item[3]))
         else:
             try:
                 now, year, month, day = getDate()
                 rate, higherNow, left, right = check_exchange(
                     command.rstrip(), year, month, day)
-              
 
                 if higherNow:
                     add_to_table(
@@ -48,27 +50,27 @@ def cli_commands():
                         left.upper(),
                         right.upper(),
                         rate,
-                        True )
+                        True)
                     print(
                         now,
                         left.upper(),
                         right.upper(),
                         prGreen(rate)
-                        )
-                if higherNow == False:
+                    )
+                if not higherNow:
                     add_to_table(
                         now,
                         left.upper(),
                         right.upper(),
                         rate,
-                        False )
+                        False)
                     print(
                         now,
                         left.upper(),
                         right.upper(),
                         prRed(rate)
-                        )
-                
+                    )
+
             except BaseException:
                 print("Wrong input")
 
--- modules/connect_db.py
+++ modules/connect_db.py
@@ -1,21 +1,28 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 
 conn = psycopg2.connect(
-        database="postgres", 
-        user='postgres', 
-        password='postgres', 
-        host='localhost', 
-        port= '5432'
-    )
-    
+    database="postgres",
+    user=POSTGRES_USER,
+    password=POSTGRES_PASSWORD,
+    host='db',
+    port='5432'
+)
+
 cursor = conn.cursor()
 
+# vytvori tabulku history_table
 def connect_to_db():
 
     cursor.execute("DROP TABLE IF EXISTS history_table")
-    
-    sql ='''CREATE TABLE history_table(
+
+    sql = '''CREATE TABLE history_table(
         date CHAR(16),
         first_currency VARCHAR(3) ,
         second_currency VARCHAR(3),
@@ -24,20 +31,22 @@ def connect_to_db():
     )'''
 
     cursor.execute(sql)
-    print("nehe")
-  
 
+# funkcia ktora zadava hodnoty do databazy
 def add_to_table(date, firstCurrency, secondCurrency, rate, higherNow):
-    sql = 'INSERT INTO history_table(date, first_currency, second_currency, rate, higher_now) VALUES (%s, %s, %s, %s, %s)'
-    cursor.execute(sql,(date, firstCurrency, secondCurrency, rate, higherNow))
-    
+    sql = '''INSERT INTO history_table
+            (date, first_currency, second_currency, rate, higher_now)
+            VALUES (%s, %s, %s, %s, %s)'''
+    cursor.execute(sql, 
+                   (date, firstCurrency, secondCurrency, rate, higherNow))
 
+# ukoncenie pirpojenia na db
 def quit_db():
     conn.commit()
     conn.close()
 
+# query vsetkych poloziek v db
 def show_history():
     cursor.execute('''SELECT * from history_table''')
-    #Fetching 1st row from the table
-    result = cursor.fetchall();
+    result = cursor.fetchall()
     return result
